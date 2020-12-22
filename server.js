22c61abe7e27fe9b4fb555740695fb287e6706a6
merikettapearl212 <68967132+merikettapearl212@users.noreply.github.com>
2020-12-21 22:11:52 -0800

    api routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 // Dependencies
 const express = require("express");
+const path = require("path");
+// require the "fs" module to import it
 const fs = require("fs");
+const { notStrictEqual } = require("assert");
 
 const app = express();
 const PORT = process.env.PORT || 1200;
 
+// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -12,26 +16,39 @@ app.use(express.static('public'));
 
 // API Routes
 app.get("/api/notes", function(req, res) {
-  //Use the fs module tp read the file
-  //THEN parse the file contents w/ JSON.parse() to real data
-  //send the parsed data back to client with res.json()
+  // Use the fs module tp read the file
+  // THEN .then() parse the file contents w/ JSON.parse() to real data 
+  // Send the parsed data back to client with res.json()
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+// Creates new note
+app.post("/api/notes", function(req, res) {
+    // Access the POSTed data in req.body
+    // use the fs module to read the file
+    // Then parse the file contents with JSON.parse() to real data
+    // Push the req.body to array list
+    // JSON.stringify() the array list back into JSON string
+    // Then save the contents back to the bd.json file w/ fs module
+});
+
+app.delete("/api/notes/:id", function(req, res) {
+    // Access :id from req.params.id
+    // Use the fs module to read the file
+    // Then parse the file contents with JSON.parse() to the real data
+});
+
 app.get("/notes", function(req, res) {
-  res.sendFile(path.join(__dirname, "/public/about.html"));
+
+  // Return the contents of the notes.html
+  res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
 app.get("*", function(req, res) {
+  // Return the contents of the index.html
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-//GET route returns all saved notes
-// app.get("/api/notes", function (req, res) {
-//   return res.json(db);
-// });
-//POST route, create new note
-//DELETE route, query parameter containing the id of a note to delete
 
 // Listener
 app.listen(PORT, function() {
